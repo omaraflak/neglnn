6481be3561e7fef6321cb21d8ffa6dda4109f7d6
Omar Aflak <aflakomar@gmail.com>
2022-06-19 20:12:31 +0100

    put xor in example/ folder

--- example/xor.py
+++ example/xor.py
@@ -4,7 +4,7 @@ from neglnn.activations.tanh import Tanh
 from neglnn.losses.mse import MSE
 from neglnn.initializers.normal import Normal
 from neglnn.optimizers.sgd import SGD
-from neglnn.network.network import fit, initialize, predict
+from neglnn.network.network import fit, predict
 
 X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 1, 2))
 Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
