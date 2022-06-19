963719d0fbee425d669461b261c3eb54dbd88210
Omar Aflak <aflakomar@gmail.com>
2022-06-19 20:50:26 +0100

    clean activation

--- neglnn/layers/activation.py
+++ neglnn/layers/activation.py
@@ -2,18 +2,18 @@ from neglnn.layers.layer import Layer, BackwardState
 from neglnn.utils.types import Array
 
 class Activation(Layer):
-    def activation(self, x: Array) -> Array:
+    def call(self, x: Array) -> Array:
         raise NotImplementedError
     
-    def activation_prime(self, x: Array) -> Array:
+    def prime(self, x: Array) -> Array:
         raise NotImplementedError
 
     def forward(self, input: Array) -> Array:
         self.input = input
-        return self.activation(input)
+        return self.call(input)
     
     def backward(self, output_gradient: Array) -> BackwardState:
-        return BackwardState(output_gradient * self.activation_prime(self.input), None)
+        return BackwardState(output_gradient * self.prime(self.input), None)
     
     def trainable(self) -> bool:
         return False
