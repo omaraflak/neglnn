9710308616fc4170b0cedd3749a6e9cbbaf03048
Omar Aflak <aflakomar@gmail.com>
2022-06-19 20:51:03 +0100

    fix activations

--- neglnn/activations/sigmoid.py
+++ neglnn/activations/sigmoid.py
@@ -3,9 +3,9 @@ from neglnn.layers.activation import Activation
 from neglnn.utils.types import Array
 
 class Sigmoid(Activation):
-    def activation(self, x: Array) -> Array:
+    def call(self, x: Array) -> Array:
         return 1 / (1 + np.exp(-x))
     
-    def activation_prime(self, x: Array) -> Array:
-        s = self.activation(x)
+    def prime(self, x: Array) -> Array:
+        s = self.call(x)
         return s * (1 - s)
--- neglnn/activations/tanh.py
+++ neglnn/activations/tanh.py
@@ -3,8 +3,8 @@ from neglnn.layers.activation import Activation
 from neglnn.utils.types import Array
 
 class Tanh(Activation):
-    def activation(self, x: Array) -> Array:
+    def call(self, x: Array) -> Array:
         return np.tanh(x)
     
-    def activation_prime(self, x: Array) -> Array:
+    def prime(self, x: Array) -> Array:
         return 1 - np.power(np.tanh(x), 2)
