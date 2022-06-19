8f2e188ed4746a3e1e402fda8c7cd7e0d199ab0a
Omar Aflak <aflakomar@gmail.com>
2022-06-19 20:45:21 +0100

    rm dep

--- example/xor.py
+++ example/xor.py
@@ -4,7 +4,6 @@ from neglnn.activations.tanh import Tanh
 from neglnn.losses.mse import MSE
 from neglnn.initializers.normal import Normal
 from neglnn.optimizers.sgd import SGD
-from neglnn.optimizers.momentum import Momentum
 from neglnn.network.network import fit, predict
 
 X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 1, 2))
