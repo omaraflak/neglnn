af96983841d3adeadfafe94602405b768dfb82b9
Omar Aflak <aflakomar@gmail.com>
2022-06-30 21:55:38 +0100

    linear activation

---
+++ neglnn/activations/linear.py
@@ -0,0 +1,13 @@
+import numpy as np
+from neglnn.layers.activation import Activation
+from neglnn.utils.types import Array, Float
+
+class Linear(Activation):
+    def __init__(self, c: Float = 1.0):
+        self.c = c
+
+    def call(self, x: Array) -> Array:
+        return self.c * x
+    
+    def prime(self, x: Array) -> Array:
+        return np.full_like(x, self.c)
