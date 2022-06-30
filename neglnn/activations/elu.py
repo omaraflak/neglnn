fa1b58579d36bda8bd3411c5006e9017ad354eb8
Omar Aflak <aflakomar@gmail.com>
2022-06-30 21:59:19 +0100

    elu layer

---
+++ neglnn/activations/elu.py
@@ -0,0 +1,13 @@
+import numpy as np
+from neglnn.layers.activation import Activation
+from neglnn.utils.types import Array, Float
+
+class LeakyRelu(Activation):
+    def __init__(self, alpha: Float = 1.0):
+        self.alpha = alpha
+
+    def call(self, x: Array) -> Array:
+        return ((x > 0) * x) + ((x <= 0) * self.alpha * (np.exp(x) - 1))
+    
+    def prime(self, x: Array) -> Array:
+        return (x > 0) + ((x <= 0) * self.alpha * np.exp(x))
